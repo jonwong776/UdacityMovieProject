872299eb5fb7352a86b0e212b489b5877f2cf645
jonwong776 <jonwong776@gmail.com>
2018-02-14 09:48:00 -0500

    Add files via upload
    Changed to make PEP8 compliant

--- media.py
+++ media.py
@@ -1,18 +1,18 @@
 class Movie():
 
-	"""Represents a movie.
+    """Represents a movie.
 
-	Used by the entertainment_center.py file to create Movie objects.
+    Used by the entertainment_center.py file to create Movie objects.
 
-	Attributes:
-		title: A string of the movie title.
-		poster_image_url: A string of the URL of the movie poster.
-		trailer_youtube_url: A string of the URL of the movie trailer.
-	"""
+    Attributes:
+        title: A string of the movie title.
+        poster_image_url: A string of the URL of the movie poster.
+        trailer_youtube_url: A string of the URL of the movie trailer.
+    """
 
-	def __init__(self, movie_title, poster_image, trailer_youtube):
+    def __init__(self, movie_title, poster_image, trailer_youtube):
 
-		# Initialization of Movie object attributes
-		self.title = movie_title
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+        # Initialization of Movie object attributes
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
